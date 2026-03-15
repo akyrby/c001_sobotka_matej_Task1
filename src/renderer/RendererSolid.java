a47a17007d9fc5ecb3fa865438f378d714cd1ccf
Matej_Sobotka <sobotma1@uhk.cz>
2026-03-15 16:56:09 +0100

    refactor RendererSolid to improve rendering logic and add vertex transformation

--- src/renderer/RendererSolid.java
+++ src/renderer/RendererSolid.java
@@ -5,25 +5,57 @@ import model.SolidPart;
 import model.Vertex;
 import rasterize.LineRasterizer;
 import rasterize.TriangleRasterizer;
+import transforms.Mat4;
+import transforms.Mat4Identity;
+import transforms.Point3D;
+import transforms.Vec3D;
+
+import java.util.Optional;
 
 public class RendererSolid {
-    private LineRasterizer lineRasterizer;
-    private TriangleRasterizer triangleRasterizer;
+    private final LineRasterizer lineRasterizer;
+    private final TriangleRasterizer triangleRasterizer;
+    private final int viewportWidth;
+    private final int viewportHeight;
+
+    private Mat4 viewMatrix = new Mat4Identity();
+    private Mat4 projectionMatrix = new Mat4Identity();
 
 
-    public RendererSolid(LineRasterizer lineRasterizer, TriangleRasterizer triangleRasterizer) {
+    public RendererSolid(LineRasterizer lineRasterizer, TriangleRasterizer triangleRasterizer, int viewportWidth, int viewportHeight) {
         this.lineRasterizer = lineRasterizer;
         this.triangleRasterizer = triangleRasterizer;
+        this.viewportWidth = viewportWidth;
+        this.viewportHeight = viewportHeight;
     }
 
-    public void render(Solid solid){
+    public RendererSolid(LineRasterizer lineRasterizer, TriangleRasterizer triangleRasterizer) {
+        this(lineRasterizer, triangleRasterizer, 800, 600);
+    }
+
+    public void setViewMatrix(Mat4 viewMatrix) {
+        this.viewMatrix = viewMatrix;
+    }
+
+    public void setProjectionMatrix(Mat4 projectionMatrix) {
+        this.projectionMatrix = projectionMatrix;
+    }
+
+    public void render(Solid solid) {
+        render(solid, solid.getModelMat());
+    }
+
+    public void render(Solid solid, Mat4 modelMatrix){
+        Mat4 mvp = modelMatrix.mul(viewMatrix).mul(projectionMatrix);
+
         for(SolidPart part : solid.getPartBuffer()){
             switch (part.getTopology()){
                 case POINTS:
                     //TODO: points
                     break;
+                case LINE_LIST:
+                case LINE_STRIP:
                 case LINES:
-                    //TODO: lines
                     int index = part.getStartIndex();
                     for(int i =0; i < part.getPrimitiveCount(); i++){
                         int indexA = solid.getIndexBuffer().get(index++);
@@ -31,25 +63,25 @@ public class RendererSolid {
 
                         Vertex a = solid.getVertexBuffer().get(indexA);
                         Vertex b = solid.getVertexBuffer().get(indexB);
-                        //TODO: Vrcholy pronásobím MVP maticí
 
-                        //TODO: Ořežání
+                        Vertex aT = transformVertex(a, mvp);
+                        Vertex bT = transformVertex(b, mvp);
+                        if (aT == null || bT == null) {
+                            continue;
+                        }
 
-                        //TODO:Dehomogenizace
-
-                        //TODO: Transformace do okna
-
-                        //TODO: Rasterizace
                         lineRasterizer.rasterize(
-                                (int) Math.round(a.getX()),
-                                (int) Math.round(a.getY()),
-                                (int) Math.round(b.getX()),
-                                (int) Math.round(b.getY())
+                                (int) Math.round(aT.getX()),
+                                (int) Math.round(aT.getY()),
+                                (int) Math.round(bT.getX()),
+                                (int) Math.round(bT.getY())
                         );
                     }
                     break;
+                case TRIANGLE_LIST:
+                case TRIANGLE_STRIP:
+                case TRIANGLE_FAN:
                 case TRIANGLES:
-                    //TODO: triangles
                         index = part.getStartIndex();
                     for(int i =0; i < part.getPrimitiveCount(); i++){
                         int indexA = solid.getIndexBuffer().get(index++);
@@ -59,20 +91,42 @@ public class RendererSolid {
                         Vertex a = solid.getVertexBuffer().get(indexA);
                         Vertex b = solid.getVertexBuffer().get(indexB);
                         Vertex c = solid.getVertexBuffer().get(indexC);
-                        //TODO: Vrcholy pronásobím MVP maticí
 
+                        Vertex aT = transformVertex(a, mvp);
+                        Vertex bT = transformVertex(b, mvp);
+                        Vertex cT = transformVertex(c, mvp);
+                        if (aT == null || bT == null || cT == null) {
+                            continue;
+                        }
 
-                        //TODO: Ořežání
-
-                        //TODO:Dehomogenizace
-
-                        //TODO: Transformace do okna
-
-                        //TODO: Rasterizace
-                        triangleRasterizer.rasterize(a, b, c);
+                        triangleRasterizer.rasterize(aT, bT, cT);
                     }
                     break;
             }
         }
+    }
+
+    private Vertex transformVertex(Vertex source, Mat4 mvp) {
+        Point3D clipPoint = source.getPosition().mul(mvp);
+        if (clipPoint.getW() <= 0.0) {
+            return null;
+        }
+
+        Optional<Vec3D> ndcOptional = clipPoint.dehomog();
+        if (!ndcOptional.isPresent()) {
+            return null;
+        }
+
+        Vec3D ndc = ndcOptional.get();
+        if (ndc.getX() < -1.0 || ndc.getX() > 1.0
+                || ndc.getY() < -1.0 || ndc.getY() > 1.0
+                || ndc.getZ() < 0.0 || ndc.getZ() > 1.0) {
+            return null;
+        }
+
+        double x = (ndc.getX() + 1.0) * 0.5 * (viewportWidth - 1);
+        double y = (1.0 - ndc.getY()) * 0.5 * (viewportHeight - 1);
+
+        return new Vertex(x, y, ndc.getZ(), source.getCol());
     }
 }
